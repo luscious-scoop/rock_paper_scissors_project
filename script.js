14b3f468586c2d28c6e79b57895c4a8ad563d1b3
Haseeb <ah085904@gmail.com>
2026-04-05 19:27:07 +0500

    new global variables for choices

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let humanScore = 0;
 let computerScore = 0;
 let result = "";
+let playerChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
 
 function getComputerChoice() {
   let randomNumber = Math.random();
