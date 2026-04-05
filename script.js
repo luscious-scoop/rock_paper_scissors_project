4fd57e12f01ebed356c8f623fabdd504e920138b
Haseeb <ah085904@gmail.com>
2026-04-05 17:52:14 +0500

    fixing the logic of random number generator , the number was equally distributed in three parts between 0 and 1

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function getComputerChoice() {
   let randomNumber = Math.random();
+  console.log(randomNumber);
   let computerChoice = "";
   if (randomNumber >= 0 && randomNumber < 1 / 4) {
     computerChoice = "Rock";
-  } else if (randomNumber >= 1 / 4 && randomNumber < 1 / 8) {
+  } else if (randomNumber >= 1 / 4 && randomNumber < 3 / 4) {
     computerChoice = "Paper";
-  } else if (randomNumber >= 1 / 8 && randomNumber < 1) {
+  } else if (randomNumber >= 3 / 4 && randomNumber < 1) {
     computerChoice = "Scissors";
   }
+
+  console.log(computerChoice);
 }
+
+getComputerChoice();
