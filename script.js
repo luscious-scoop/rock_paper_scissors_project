ecdfd0a9eb2fb61435802783d01e55b729526e26
Haseeb <ah085904@gmail.com>
2026-04-06 17:18:44 +0500

    better split ranges for 0 and 1 for equal chance for computer choosing each option

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ function getComputerChoice() {
   let randomNumber = Math.random();
 
   let computerChoice = "";
-  if (randomNumber >= 0 && randomNumber < 1 / 4) {
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
     computerChoice = "Rock";
-  } else if (randomNumber >= 1 / 4 && randomNumber < 3 / 4) {
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
     computerChoice = "Paper";
-  } else if (randomNumber >= 3 / 4 && randomNumber < 1) {
+  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
     computerChoice = "Scissors";
   }
 
